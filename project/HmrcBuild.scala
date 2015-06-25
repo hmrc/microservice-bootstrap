0f55f7bcf18efbec6193a33e6404229ff6a41018
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2015-06-25 10:59:33 +0100

    [MDTP-114] remove objects that will be created by services in their wiring code

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -26,23 +26,13 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   val httpVerbsVersion = "1.7.0"
-  val playAuthorisationVersion = "0.14.0"
 
   val compile = Seq(
     "com.typesafe.play" %% "play" % PlayVersion.current,
-
-//    "com.kenshoo" %% "metrics-play" % "2.3.0_0.1.8",
     "uk.gov.hmrc" %% "play-config" % "1.0.0",
-    "uk.gov.hmrc" %% "http-verbs" % httpVerbsVersion,
-    "uk.gov.hmrc" %% "play-authorisation" % playAuthorisationVersion,
+    "uk.gov.hmrc" %% "http-verbs" % "1.7.0",
     "uk.gov.hmrc" %% "play-filters" % "1.2.0",
-    "uk.gov.hmrc" %% "play-graphite" % "1.0.0",
-    "uk.gov.hmrc" %% "play-json-logger" % "1.0.0"
-//    "commons-io" % "commons-io" % "2.4",
-//    "net.ceedubs" %% "ficus" % "1.1.1",
-//    "uk.gov.hmrc" %% "time" % "1.1.0",
-//    "uk.gov.hmrc" %% "domain" % "2.6.0",
-//    "uk.gov.hmrc" % "secure-utils" % "5.1.0"
+    "uk.gov.hmrc" %% "play-graphite" % "1.0.0"
   )
 
   trait TestDependencies {
@@ -54,11 +44,8 @@ private object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-//        "commons-codec" % "commons-codec" % "1.8.0" % scope,
         "org.scalatest" %% "scalatest" % "2.2.2" % scope,
         "org.pegdown" % "pegdown" % "1.4.2" % scope,
-//        "com.github.tomakehurst" % "wiremock" % "1.48" % scope,
-//        "uk.gov.hmrc" %% "http-verbs" % httpVerbsVersion % scope classifier "tests",
         "uk.gov.hmrc" %% "hmrctest" % "1.0.0" % scope
       )
     }.test
--- src/main/scala/uk/gov/hmrc/play/bootstrap/Global.scala
+++ src/main/scala/uk/gov/hmrc/play/bootstrap/Global.scala
@@ -1,33 +1,36 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.play.bootstrap
 
 import com.typesafe.config.Config
 import net.ceedubs.ficus.Ficus._
 import play.api._
 import play.api.mvc._
-import uk.gov.hmrc.play.audit.filters.{AuditFilter => AuditFiltering}
-import uk.gov.hmrc.play.audit.http.config.{ErrorAuditingSettings, LoadAuditingConfig}
-import uk.gov.hmrc.play.audit.http.connector.{AuditConnector => Auditing}
-import uk.gov.hmrc.play.config.{AppName, ControllerConfig, RunMode}
+import uk.gov.hmrc.play.audit.filters.AuditFilter
+import uk.gov.hmrc.play.audit.http.config.ErrorAuditingSettings
+import uk.gov.hmrc.play.config.{AppName, ControllerConfig}
 import uk.gov.hmrc.play.filters._
 import uk.gov.hmrc.play.graphite.GraphiteConfig
 import uk.gov.hmrc.play.http.logging.filters.LoggingFilter
-import uk.gov.hmrc.play.http.ws.{WSDelete, WSGet, WSPatch, WSPost, WSPut}
 
-object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName with RunMode {
-  override val auditConnector = AuditConnector
-}
+trait MicroserviceFilters {
 
-object AuditConnector extends Auditing with AppName with RunMode {
-  override lazy val auditingConfig = LoadAuditingConfig(s"$env.auditing")
-}
-
-
-object DefaultFilters {
-
-  object AuditFilter extends AuditFiltering with AppName {
-    override val auditConnector = AuditConnector
-    override def controllerNeedsAuditing(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsAuditing
-  }
+  def auditFilter:AuditFilter
+  def loggingFilter:LoggingFilter
 
   object MicroserviceLoggingFilter extends LoggingFilter {
     override def controllerNeedsLogging(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsLogging
@@ -37,30 +40,32 @@ object DefaultFilters {
     lazy val controllerConfigs = Play.current.configuration.underlying.as[Config]("controllers")
   }
 
-  def apply(): Seq[EssentialFilter] = Seq(
-    AuditFilter,
-    MicroserviceLoggingFilter,
+  def defaultFilters: Seq[EssentialFilter] = Seq(
+    auditFilter,
+    loggingFilter,
     NoCacheFilter,
     RecoveryFilter)
 }
 
-abstract class DefaultGlobal(filters: Seq[EssentialFilter]) extends WithFilters(filters: _*)  with GraphiteConfig with AppName {
+abstract class DefaultMicroserviceGlobal
+  extends GlobalSettings
+  with MicroserviceFilters
+  with GraphiteConfig
+  with AppName
+  with ErrorAuditingSettings
+  with JsonErrorHandling {
 
   override def onStart(app: Application) {
     Logger.info(s"Starting microservice : $appName : in mode : ${app.mode}")
     super.onStart(app)
   }
 
+  override def doFilter(a: EssentialAction): EssentialAction = {
+    Filters(super.doFilter(a), defaultFilters: _*)
+  }
+
   // Play 2.0 doesn't support trailing slash: http://play.lighthouseapp.com/projects/82401/tickets/98
   override def onRouteRequest(request: RequestHeader) = super.onRouteRequest(request).orElse {
     Some(request.path).filter(_.endsWith("/")).flatMap(p => super.onRouteRequest(request.copy(path = p.dropRight(1))))
   }
-}
-
-abstract class RestGlobal extends DefaultGlobal(DefaultFilters()) with JsonErrorHandling with ErrorAuditingSettings
-
-object RestGlobal extends RestGlobal with RunMode {
-  override val auditConnector = AuditConnector
-
-  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"$env.microservice.metrics")
 }
