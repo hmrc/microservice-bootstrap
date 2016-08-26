0222fbd92592f12819022ce965a003187818ee68
Christopher Townson <iteratoruk@gmail.com>
2016-08-26 14:10:32 +0100

    Use Play current version reference in build when declaring Play library deps

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,3 +1,4 @@
+import play.core.PlayVersion
 import sbt.Keys._
 import sbt._
 
@@ -39,7 +40,7 @@ object Dependencies {
   )
 
   val test = Seq(
-    "com.typesafe.play" %% "play-test" % "2.5.3" % "test",
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % "test",
     "org.scalatest" %% "scalatest" % "2.2.4" % "test",
     "org.pegdown" % "pegdown" % "1.5.0" % "test",
     "org.mockito" % "mockito-all" % "1.9.5" % "test"
