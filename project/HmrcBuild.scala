0a698911059a46e9861ef75cbef55c2673c63f57
Christopher Townson <iteratoruk@gmail.com>
2016-10-20 13:23:23 +0100

    TXMNT-366 bump play-filters after http-verbs version increase

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -33,7 +33,7 @@ object Dependencies {
 
   val compile = Seq(
     filters,
-    "uk.gov.hmrc" %% "play-filters" % "5.0.0",
+    "uk.gov.hmrc" %% "play-filters" % "5.1.0",
     "uk.gov.hmrc" %% "play-graphite" % "3.0.0",
     "com.typesafe.play" %% "play" % "2.5.8",
     "de.threedimensions" %% "metrics-play" % "2.5.13"
