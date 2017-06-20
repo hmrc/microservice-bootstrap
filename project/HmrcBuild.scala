1ecdf7d55bd2afd9090a58069e56b99894c22918
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2017-06-20 07:57:55 +0100

    [PLATOPS-1017] upping the play-filters to 5.18.0 (so http-verbs upped to 6.4.0)

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -33,7 +33,7 @@ object Dependencies {
 
   val compile = Seq(
     filters,
-    "uk.gov.hmrc" %% "play-filters" % "5.15.0",
+    "uk.gov.hmrc" %% "play-filters" % "5.18.0",
     "uk.gov.hmrc" %% "play-graphite" % "3.2.0",
     "com.typesafe.play" %% "play" % "2.5.12",
     "de.threedimensions" %% "metrics-play" % "2.5.13",
