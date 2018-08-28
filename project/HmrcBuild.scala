44db390e03141bb3b5a24466b15ee0254d034789
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2018-08-28 11:24:00 +0100

    PLATOPS-1550 - upgraded http-verbs-play-25

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -35,7 +35,7 @@ object Dependencies {
     filters,
     "uk.gov.hmrc"        %% "crypto"              % "4.5.0",
     "uk.gov.hmrc"        %% "http-verbs"          % "7.3.0",
-    "uk.gov.hmrc"        %% "http-verbs-play-25"  % "0.10.0",
+    "uk.gov.hmrc"        %% "http-verbs-play-25"  % "0.11.0",
     "uk.gov.hmrc"        %% "play-auditing"       % "3.3.0",
     "uk.gov.hmrc"        %% "play-graphite"       % "3.6.2",
     "uk.gov.hmrc"        %% "play-config"         % "4.3.3",
