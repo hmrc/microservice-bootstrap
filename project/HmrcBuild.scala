598a3375f5822d0115708e93aba4efae145f4ea2
Dario Abdulrehman <dario.rehman@gmail.com>
2018-10-12 09:21:38 +0100

    PLATOPS-1620: Force upgrade of jackson dependencies
    Due to a security flaw found via XRay on jackson-databind 2.7.8 we force
    the upgrade to jackson 2.9.7.

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -33,19 +33,24 @@ object Dependencies {
 
   val compile = Seq(
     filters,
-    "uk.gov.hmrc"        %% "crypto"              % "4.5.0",
-    "uk.gov.hmrc"        %% "http-verbs"          % "7.3.0",
-    "uk.gov.hmrc"        %% "http-verbs-play-25"  % "0.12.0",
-    "uk.gov.hmrc"        %% "play-auditing"       % "3.9.0-play-25",
-    "uk.gov.hmrc"        %% "play-graphite"       % "3.6.2",
-    "uk.gov.hmrc"        %% "play-config"         % "4.3.3",
-    "uk.gov.hmrc"        %% "play-authorisation"  % "5.1.0",
-    "uk.gov.hmrc"        %% "play-health"         % "2.1.0",
-    "ch.qos.logback"     % "logback-core"         % "1.1.7",
-    "uk.gov.hmrc"        %% "logback-json-logger" % "3.1.0",
-    "com.typesafe.play"  %% "play"                % "2.5.12",
-    "de.threedimensions" %% "metrics-play"        % "2.5.13",
-    "ch.qos.logback"     % "logback-core"         % "1.1.7"
+    "uk.gov.hmrc"                    %% "crypto"                 % "4.5.0",
+    "uk.gov.hmrc"                    %% "http-verbs"             % "7.3.0",
+    "uk.gov.hmrc"                    %% "http-verbs-play-25"     % "0.12.0",
+    "uk.gov.hmrc"                    %% "play-auditing"          % "3.9.0-play-25",
+    "uk.gov.hmrc"                    %% "play-graphite"          % "3.6.2",
+    "uk.gov.hmrc"                    %% "play-config"            % "4.3.3",
+    "uk.gov.hmrc"                    %% "play-authorisation"     % "5.1.0",
+    "uk.gov.hmrc"                    %% "play-health"            % "2.1.0",
+    "ch.qos.logback"                 % "logback-core"            % "1.1.7",
+    "uk.gov.hmrc"                    %% "logback-json-logger"    % "3.1.0",
+    "com.typesafe.play"              %% "play"                   % "2.5.12",
+    "de.threedimensions"             %% "metrics-play"           % "2.5.13",
+    "ch.qos.logback"                 % "logback-core"            % "1.1.7",
+    "com.fasterxml.jackson.core"     % "jackson-core"            % "2.9.7" force (),
+    "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.7" force (),
+    "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.7" force (),
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.7" force (),
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7" force ()
   )
 
   val test = Seq(
