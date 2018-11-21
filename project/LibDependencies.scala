540eb4b24c3e12952b44162a098dc517734df08e
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2018-11-21 16:07:07 +0000

    PLATOPS-1619: Upgraded to play-2.5.19

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -4,26 +4,29 @@ import sbt._
 
 object LibDependencies {
 
+  val akkaVersion = "2.5.18"
+
   val compile = Seq(
     filters,
+    "com.typesafe.akka"              %% "akka-actor"             % akkaVersion,
+    "com.typesafe.akka"              %% "akka-stream"            % akkaVersion,
+    "com.typesafe.akka"              %% "akka-slf4j"             % akkaVersion,
     "uk.gov.hmrc"                    %% "crypto"                 % "4.5.0",
-    "uk.gov.hmrc"                    %% "http-verbs"             % "7.3.0",
-    "uk.gov.hmrc"                    %% "http-verbs-play-25"     % "0.12.0",
-    "uk.gov.hmrc"                    %% "play-auditing"          % "3.12.0-play-25",
-    "uk.gov.hmrc"                    %% "play-graphite"          % "3.6.2",
-    "uk.gov.hmrc"                    %% "play-config"            % "4.3.3",
-    "uk.gov.hmrc"                    %% "play-authorisation"     % "5.1.0",
-    "uk.gov.hmrc"                    %% "play-health"            % "2.1.0",
-    "ch.qos.logback"                 % "logback-core"            % "1.1.7",
-    "uk.gov.hmrc"                    %% "logback-json-logger"    % "3.1.0",
-    "com.typesafe.play"              %% "play"                   % "2.5.12",
+    "uk.gov.hmrc"                    %% "http-verbs"             % "8.10.0-play-25-SNAPSHOT",
+    "uk.gov.hmrc"                    %% "play-auditing"          % "3.14.0-play-25-SNAPSHOT",
+    "uk.gov.hmrc"                    %% "play-graphite"          % "4.4.0-SNAPSHOT",
+    "uk.gov.hmrc"                    %% "play-config"            % "7.0.0-1-gbc31795",
+    "uk.gov.hmrc"                    %% "play-authorisation"     % "5.1.0-3-g8196245",
+    "uk.gov.hmrc"                    %% "play-health"            % "3.9.0-play-25-SNAPSHOT",
+    "ch.qos.logback"                 % "logback-classic"         % "1.2.3",
+    "uk.gov.hmrc"                    %% "logback-json-logger"    % "4.1.0",
+    "com.typesafe.play"              %% "play"                   % PlayVersion.current,
     "de.threedimensions"             %% "metrics-play"           % "2.5.13",
-    "ch.qos.logback"                 % "logback-core"            % "1.1.7",
-    "com.fasterxml.jackson.core"     % "jackson-core"            % "2.9.7" force (),
-    "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.7" force (),
-    "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.7" force (),
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.7" force (),
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7" force ()
+    "com.fasterxml.jackson.core"     % "jackson-core"            % "2.9.7",
+    "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.7",
+    "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.7",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.7",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7"
   )
 
   val test = Seq(
@@ -31,7 +34,7 @@ object LibDependencies {
     "com.typesafe.play"      %% "play-specs2"        % PlayVersion.current % "test",
     "org.pegdown"            % "pegdown"             % "1.5.0"             % "test",
     "org.mockito"            % "mockito-all"         % "1.9.5"             % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1"             % "test",
-    "uk.gov.hmrc"            %% "hmrctest"           % "2.3.0"             % "test"
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1"             % "test",
+    "uk.gov.hmrc"            %% "hmrctest"           % "3.3.0-SNAPSHOT"    % "test"
   )
 }
