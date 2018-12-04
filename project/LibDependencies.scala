765c2d9657607d0c5dd2f6f523d0dc30fbf5625b
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2018-12-04 10:48:48 +0000

    PLATOPS-1619: upgrade to Play 2.5.19

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -12,12 +12,12 @@ object LibDependencies {
     "com.typesafe.akka"              %% "akka-stream"            % akkaVersion,
     "com.typesafe.akka"              %% "akka-slf4j"             % akkaVersion,
     "uk.gov.hmrc"                    %% "crypto"                 % "4.5.0",
-    "uk.gov.hmrc"                    %% "http-verbs"             % "8.10.0-play-25-SNAPSHOT",
-    "uk.gov.hmrc"                    %% "play-auditing"          % "3.14.0-play-25-SNAPSHOT",
+    "uk.gov.hmrc"                    %% "http-verbs"             % "8.10.0-play-25",
+    "uk.gov.hmrc"                    %% "play-auditing"          % "3.14.0-play-25",
     "uk.gov.hmrc"                    %% "play-graphite"          % "4.4.0-SNAPSHOT",
-    "uk.gov.hmrc"                    %% "play-config"            % "7.0.0-1-gbc31795",
-    "uk.gov.hmrc"                    %% "play-authorisation"     % "5.1.0-3-g8196245",
-    "uk.gov.hmrc"                    %% "play-health"            % "3.9.0-play-25-SNAPSHOT",
+    "uk.gov.hmrc"                    %% "play-config"            % "7.2.0",
+    "uk.gov.hmrc"                    %% "play-authorisation"     % "5.1.0",
+    "uk.gov.hmrc"                    %% "play-health"            % "3.9.0-play-25",
     "ch.qos.logback"                 % "logback-classic"         % "1.2.3",
     "uk.gov.hmrc"                    %% "logback-json-logger"    % "4.1.0",
     "com.typesafe.play"              %% "play"                   % PlayVersion.current,
@@ -35,6 +35,6 @@ object LibDependencies {
     "org.pegdown"            % "pegdown"             % "1.5.0"             % "test",
     "org.mockito"            % "mockito-all"         % "1.9.5"             % "test",
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1"             % "test",
-    "uk.gov.hmrc"            %% "hmrctest"           % "3.3.0-SNAPSHOT"    % "test"
+    "uk.gov.hmrc"            %% "hmrctest"           % "3.3.0"    % "test"
   )
 }
